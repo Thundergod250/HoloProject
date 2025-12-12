c7197a688f30b549862a8076f75859148bbe4443
masterdavid250 <davidarielconcon@gmail.com>
2025-12-12 14:02:24 +0800

    Added Player Animation

---
+++ Assets/Scripts/Scripts_UI/UI_TowerUpgradeCard.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TowerUpgradeCard : MonoBehaviour
+{
+    public Image CardImage;
+    public TextMeshProUGUI cardInfo;
+    public Button upgradeButton; 
+}
