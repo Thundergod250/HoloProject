778db7e039787b1302db664b24f9241d13383de5
masterdavid250 <davidarielconcon@gmail.com>
2025-12-16 15:06:50 +0800

    Removed Debug on FocusUI

--- Assets/Scripts/Scripts_UI/FocusUI.cs
+++ Assets/Scripts/Scripts_UI/FocusUI.cs
@@ -7,12 +7,5 @@ public class FocusUI : MonoBehaviour
 
     public void _FocusTowerUpgrades() => GameManager.Instance.UIManager?.FocusTowerUpgrades();
 
-    public void _FocusTowerUpgradesWithCondition(TowerController towerController)
-    {
-        Debug.Log(towerController != null
-            ? $"[FocusUI] TowerController passed: {towerController.name}"
-            : "[FocusUI] TowerController is NULL");
-
-        GameManager.Instance.UIManager?.FocusTowerUpgradesWithCondition(towerController);
-    }
+    public void _FocusTowerUpgradesWithCondition(TowerController towerController) => GameManager.Instance.UIManager?.FocusTowerUpgradesWithCondition(towerController);
 }
