e5573eabd18562745705e59d007fac0a81c5ca7b
Dragonus <davidarielconcon@gmail.com>
2025-12-18 16:15:14 +0800

    Improved TrashPile Script

--- Assets/Scripts/Scripts_GarbageObjects/TrashCollectedFromPile.cs
+++ Assets/Scripts/Scripts_GarbageObjects/TrashCollectedFromPile.cs
@@ -1,26 +1,34 @@
-using NUnit.Framework;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TrashCollectedFromPile : MonoBehaviour
 {
-    public GameObject TrashObject;
-    public GrabManipulator GrabManipulatorVar;
+    public UnityEvent<GameObject> EvtOnTrashPileInteract;
+
+    [Header("Trash Prefabs")]
+    [SerializeField] private GameObject[] trashPrefabs;
+
+    [Header("Settings")]
     [SerializeField] private int totalTrashCount = 3;
 
-    private void Update()
+    public void _InteractTrashPile()
     {
-        if(totalTrashCount <= 0)
-            Destroy(this.gameObject);
-    }
-    public void TrashTaken()
-    {
-        if (totalTrashCount > 0)
-        {
-            //Creates Instance of Trash for player to use
-            GameObject newTrash = Instantiate(TrashObject);
-            GrabManipulatorVar._Grab(newTrash);
-            totalTrashCount--;
-        } 
+        if (totalTrashCount <= 0 || trashPrefabs.Length == 0) return;
+
+        // Pick random prefab
+        GameObject prefab = trashPrefabs[Random.Range(0, trashPrefabs.Length)];
+
+        // Spawn via ObjectPooling
+        GameObject newTrash = GameManager.Instance.SpawnObject(prefab, null, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+
+        // Notify listeners
+        EvtOnTrashPileInteract?.Invoke(newTrash);
+
+        // Reduce count
+        totalTrashCount--;
+
+        // Return pile to pool if empty
+        if (totalTrashCount <= 0)
+            GameManager.Instance.ObjectPooling.Return(gameObject, gameObject);
     }
 }
