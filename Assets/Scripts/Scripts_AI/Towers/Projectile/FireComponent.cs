28e27ce886102b4796869039ba3ef32d05bf4dfb
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2025-12-19 16:14:49 +0800

    Made Healing tower for player
    - Deleted Fire Component need to rework to follow New Projectile Logic

--- Assets/Scripts/Scripts_AI/Towers/Projectile/FireComponent.cs
+++ Assets/Scripts/Scripts_AI/Towers/Projectile/FireComponent.cs
@@ -10,45 +10,5 @@ public class FireComponent : MonoBehaviour
 
     [SerializeField] protected EnemyBase enemyBase;
 
-    private int _currentTickCount;
-
-    private async void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<EnemyBase>())
-        {
-            EnemyBase enemyBase = other.GetComponent<EnemyBase>();
-
-            if (enemyBase != null)
-            {
-                await ApplyDamageOverTime(enemyBase);
-            }
-        }
-
-        else if (other.GetComponent<FireComponent>())
-        {
-            FireComponent fire = other.GetComponent<FireComponent>();
-            _currentTickCount = 0;
-            damagePerTick = fire.damagePerTick;
-            intervalDelay = fire.intervalDelay;
-            totalTicks = fire.totalTicks;
-
-            await ApplyDamageOverTime(enemyBase);
-        }
-    }
-
-    private async Task ApplyDamageOverTime(EnemyBase enemy)
-    {
-        _currentTickCount = 0;
-
-        while (_currentTickCount < totalTicks)
-        {
-            if (enemy == null) break; // Stop if enemy is destroyed
-
-            enemy.Health.TakeDamage(damagePerTick); // Assuming EnemyBase has TakeDamage(int amount)
-            _currentTickCount++;
-
-            await Task.Delay((int)(intervalDelay * 1000)); // Convert seconds to milliseconds
-        }
-    }
 }
 
