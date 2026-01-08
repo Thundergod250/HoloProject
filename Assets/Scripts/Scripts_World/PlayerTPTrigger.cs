6cc15e49f569711f064aa9a2611fcb100d02d17e
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2026-01-08 23:31:03 +0800

    Added Collisions to most Props
    + Trees, Pillars, 3 Rock
    + TP Trigger, for player or resource fell teleports back to Spawn Point
    + Nerfed a bit more the Grass Culling, now 3%

---
+++ Assets/Scripts/Scripts_World/PlayerTPTrigger.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerTPTrigger : MonoBehaviour
+{
+    [SerializeField] Transform _spawnPoint;
+    [SerializeField] PlayerController _playerController;
+    [SerializeField] GarbageObject _garbageObject;
+
+    [SerializeField] GameObject _hideUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            _hideUI.SetActive(true);
+            _playerController = other.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerController>()) 
+        {
+            _hideUI.SetActive(true);
+            _playerController.gameObject.transform.position = _spawnPoint.position;
+            _hideUI.SetActive(false);
+        }
+        else if (other.GetComponent<GarbageObject>())
+        {
+            _garbageObject = other.GetComponent<GarbageObject>();
+            //_garbageObject.transform.position = _playerController.gameObject.transform.position;
+             _garbageObject.transform.position = _spawnPoint.position;
+        }
+        
+    }
+
+}
