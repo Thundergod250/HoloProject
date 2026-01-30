1204f0a34a7a6c1dc6db039e64d7e07c1a3ecba4
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2026-01-31 01:01:34 +0800

    Beta Tower Grid system
    + Ended up wih the faster way, having placements to put towers

--- Assets/Scripts/Scripts_Interface/HighlightableToggle.cs
+++ Assets/Scripts/Scripts_Interface/HighlightableToggle.cs
@@ -2,23 +2,48 @@ using UnityEngine;
 
 public class HighlightableToggle : MonoBehaviour
 {
-    private Outline outline;
-    private void Start()
-    {
-        // Get the Outline component we added in Step 2
-        outline = GetComponent<Outline>();
+    public float interactDistance = 5f; // How far away can you see it?
+    private Outline lastOutline;
 
-        // Ensure it's off when the game starts
-        outline.enabled = false;
+    private void LateUpdate()
+    {
+        // 1. Create a ray from the center of the screen
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        // 2. Check if the ray hits anything within distance
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            // 3. Try to find the Outline component on what we hit
+            Outline outline = hit.collider.GetComponent<Outline>();
+
+            if (outline != null)
+            {
+                // If we hit a NEW object, turn off the old one and turn on the new one
+                if (lastOutline != outline)
+                {
+                    ClearHighlight();
+                    outline.enabled = true;
+                    lastOutline = outline;
+                }
+            }
+            else
+            {
+                ClearHighlight();
+            }
+        }
+        else
+        {
+            ClearHighlight();
+        }
     }
 
-    private void OnMouseEnter()
+    private void ClearHighlight()
     {
-        outline.enabled = true;
-    }
-
-    private void OnMouseExit()
-    {
-        outline.enabled = false;
+        if (lastOutline != null)
+        {
+            lastOutline.enabled = false;
+            lastOutline = null;
+        }
     }
 }
