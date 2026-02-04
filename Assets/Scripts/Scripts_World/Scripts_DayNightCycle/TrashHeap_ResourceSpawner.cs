0affba45f3f1855600e751c39fd0bbba003355d3
Jeremy Tan <jeremykurt.tan@benilde.edu.ph>
2026-02-05 00:23:05 +0800

    Temp FIx

--- Assets/Scripts/Scripts_World/Scripts_DayNightCycle/TrashHeap_ResourceSpawner.cs
+++ Assets/Scripts/Scripts_World/Scripts_DayNightCycle/TrashHeap_ResourceSpawner.cs
@@ -1,187 +1,133 @@
-using UnityEngine;
-using System.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TrashHeap_ResourceSpawner : MonoBehaviour
 {
-    [SerializeField] protected List<GameObject> _resources; // will be prefabs
+    [SerializeField] protected List<GameObject> _resources;
     [SerializeField] protected float _spawnDelaySeconds = 2f;
     [SerializeField] protected float _upwardForce = 2f;
 
-    [SerializeField] protected int _howManyToSpawn = 1; //= Random.Range(1, 5);
+    [SerializeField] protected int _howManyToSpawn = 1;
 
     [SerializeField] public GarbageObject.GarbageGroup _garbageGroupType;
     [SerializeField] public bool _randomized = false;
 
     [SerializeField] public Health _health;
     [SerializeField] private Slider _healthSlider;
-    [SerializeField] private PlayerController _playerController;
+    private PlayerController _playerController;
 
     [SerializeField] bool ForTesting = false;
-    [SerializeField] bool hasSpawned = true;
+    private bool hasSpawned = false; // Flag to prevent multiple triggerings
 
     private void Start()
     {
-        _healthSlider.maxValue = _health.GetMaxHealth();
-        _healthSlider.gameObject.SetActive(false);
+        if (_healthSlider != null)
+        {
+            _healthSlider.maxValue = _health.GetMaxHealth();
+            _healthSlider.gameObject.SetActive(false);
+        }
     }
 
-    private async void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        // Example condition: only trigger for objects tagged "Player"
-        // || (other.GetComponent<NPC_MinerMovement>()) if going to disable these things
-        if ( (other.GetComponent<PlayerController>() ))
+        if (other.TryGetComponent<PlayerController>(out var player))
         {
-            _playerController = other.GetComponent<PlayerController>();
-            SetRandomized();
-
+            _playerController = player;
+            SetRandomized(); // Fixed logic inside this function
             _healthSlider.gameObject.SetActive(true);
 
             _howManyToSpawn = Random.Range(1, 5);
 
-            if (ForTesting)
+            if (ForTesting && !hasSpawned)
             {
-                Debug.Log("Player Near Heap " + gameObject.name);
-                await SpawnResourceWithDelay();
-
-                DisableThisHeap();
+                StartCoroutine(SpawnSequence());
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if ((other.GetComponent<PlayerController>()))
+        if (other.GetComponent<PlayerController>())
         {
             _playerController = null;
             _healthSlider.gameObject.SetActive(false);
         }
     }
 
-
     private void SetRandomized()
     {
-        int testRandom = Random.Range(1, 2);
-
-        if (testRandom == 1)
-        {
-            _randomized = true;
-        }
-        else if (testRandom == 2)
-        {
-            _randomized = false;
-        }
+        // Random.Range(int, int) max is EXCLUSIVE. 
+        // To get 1 or 2, you must use (1, 3).
+        int testRandom = Random.Range(1, 3);
+        _randomized = (testRandom == 1);
     }
 
-    private async void Update()
+    private void Update()
     {
         _healthSlider.value = _health.GetCurrentHealth();
 
-        if (_health.GetCurrentHealth() <= 0)
+        if (_health.GetCurrentHealth() <= 0 && !hasSpawned)
         {
-            if (hasSpawned)
-            {
-                for (int i =0; i<_howManyToSpawn; i++)
-                {
-                    await SpawnResourceWithDelay();
-                }
-
-                DisableThisHeap();
-            }
+            StartCoroutine(SpawnSequence());
         }
 
-        if(_playerController != null)
+        if (_playerController != null && _healthSlider.gameObject.activeSelf)
         {
-            _healthSlider.gameObject.transform.LookAt(_playerController.gameObject.transform.position);
+            _healthSlider.transform.LookAt(_playerController.transform.position);
         }
     }
 
+    // This handles the timing and the loop
+    private IEnumerator SpawnSequence()
+    {
+        hasSpawned = true; // Block further execution
+
+        for (int i = 0; i < _howManyToSpawn; i++)
+        {
+            yield return new WaitForSeconds(_spawnDelaySeconds);
+            SetResourceType(); // Calls your specific logic
+        }
+
+        DisableThisHeap();
+    }
 
     private void DisableThisHeap()
     {
-        hasSpawned = false;
         this.gameObject.SetActive(false);
-    }
-
-    public void EnableThisHeap()
-    {
-        this.gameObject.SetActive(true);
-        _health.ReviveHealth();
-        //_health.Heal(_health.startSetHealth);
-    }
-
-    private async Task SpawnResourceWithDelay()
-    {
-        // Convert seconds to milliseconds for Task.Delay
-        int delayMs = (int)(_spawnDelaySeconds * 1000);
-
-        await Task.Delay(delayMs);
-
-        //if (this == null) return;
-
-        SetResourceType();
     }
 
     public void SetResourceType()
     {
+        Debug.Log("Set Resource Logic Running");
         if (!_randomized)
         {
-            if (_garbageGroupType == GarbageObject.GarbageGroup.Plastic)
-            {
-                SpawnResource(0);
-            }
-            else if (_garbageGroupType == GarbageObject.GarbageGroup.Organic)
-            {
-                SpawnResource(1);
-            }
-            else if (_garbageGroupType == GarbageObject.GarbageGroup.Metal)
-            {
-                SpawnResource(2);
-            }
+            // Use your Enum-based logic
+            if (_garbageGroupType == GarbageObject.GarbageGroup.Plastic) SpawnResource(0);
+            else if (_garbageGroupType == GarbageObject.GarbageGroup.Organic) SpawnResource(1);
+            else if (_garbageGroupType == GarbageObject.GarbageGroup.Metal) SpawnResource(2);
         }
-
-        else if (_randomized)
+        else
         {
-            int randomType = Random.Range(0, 100);
-            if (randomType <= 20 && randomType !> 20) // Plastic
-            {
-                SpawnResource(0);
-            }
-            else if (randomType <= 40 && randomType !> 40) // Wood
-            {
-                SpawnResource(1);
-            }
-            else if (randomType <= 60 && randomType !> 60) // Metal
-            {
-                SpawnResource(2);
-            }
+            int randomType = Random.Range(0, 101); // 0 to 100
+
+            // Fixed the range logic and removed the invalid !> operator
+            if (randomType <= 33) SpawnResource(0);
+            else if (randomType <= 66) SpawnResource(1);
+            else SpawnResource(2);
         }
     }
 
-
     public void SpawnResource(int targetType)
     {
-        if (_resources == null || _resources.Count == 0) return;
+        if (_resources == null || _resources.Count <= targetType) return;
 
-        GameObject prefab = null;
-
-        // 1. Pick a resource
-        if (!_randomized)
-        {
-            prefab = _resources[targetType];
-        }
-        else if (_randomized)
-        {
-            prefab = _resources[Random.Range(0, _resources.Count)];
-        }
-        // 2. Spawn the object
+        GameObject prefab = _resources[targetType];
         GameObject spawnedObj = Instantiate(prefab, transform.position, Quaternion.identity);
 
-        // 3. Apply force if it has a Rigidbody
         if (spawnedObj.TryGetComponent<Rigidbody>(out Rigidbody rb))
         {
-            // ForceMode.Impulse is best for instant "burst" movements
             rb.AddForce(Vector3.up * _upwardForce, ForceMode.Impulse);
         }
     }
