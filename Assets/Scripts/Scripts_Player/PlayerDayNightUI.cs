547f297c876299536d3465001ce1414a2abae0a6
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2026-02-04 12:58:33 +0800

    Spawner Prefab needs rework from Jeremy, DayNight UI updated and tweaked
    + Day Time 0 - 220 instead of 180 / 180
    + UI now updates, but have beta of newer system when UI swap
    
    - Bugs Heaps don't spawn or revive for some reason
    - Heaps also don't spawn anything

--- Assets/Scripts/Scripts_Player/PlayerDayNightUI.cs
+++ Assets/Scripts/Scripts_Player/PlayerDayNightUI.cs
@@ -6,11 +6,36 @@ public class PlayerDayNightUI : MonoBehaviour
 {
     [SerializeField] LightingManager lightingManager;
     [SerializeField] TextMeshProUGUI timeTextUGUI;
+    [SerializeField] TextMeshProUGUI dayStatusTextUGUI;
+
     [SerializeField] Slider dayNightslider;
+
+    [SerializeField] private GameObject _dayUI;
+    [SerializeField] private GameObject _nightUI;
 
     private void Update()
     {
         UpdateUIDayNight();
+        ChangeDayNightUI();
+    }
+
+    private void ChangeDayNightUI()
+    {
+        if (!lightingManager._isNight)
+        {
+            //_dayUI?.gameObject.SetActive(true);
+            //_nightUI?.gameObject.SetActive(false);
+
+            dayStatusTextUGUI.text = "Day Time";
+
+        }
+        else if (lightingManager._isNight)
+        {
+            //_dayUI?.gameObject.SetActive(false);
+            //_nightUI?.gameObject.SetActive(true);
+
+            dayStatusTextUGUI.text = "Night Time";
+        }
     }
 
     private void UpdateUIDayNight()
