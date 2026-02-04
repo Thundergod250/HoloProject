e2cdb0f224613cc95678026c3a06e8b7433e262f
Jeremy Tan <jeremykurt.tan@benilde.edu.ph>
2026-02-04 22:29:37 +0800

    Adjusted Enemy Death scriupt

--- Assets/Scripts/Scripts_AI/Health.cs
+++ Assets/Scripts/Scripts_AI/Health.cs
@@ -53,7 +53,7 @@ public class Health : MonoBehaviour
 
         Debug.Log($"{gameObject.name} has died.");
         // Optional: Destroy(gameObject); or disable
-        Destroy(gameObject);
+        
     }
     
     public void Heal(int amount)
