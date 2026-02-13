19244cbeea5ad93edbe3f5b34413e0cb1badd696
Jeremy Kurt Tan <jeremykurttan@yahoo.com>
2026-02-13 09:05:52 +0800

    Caution UI on Waves

--- Assets/Scripts/Scripts_AI/Spawner/Spawner.cs
+++ Assets/Scripts/Scripts_AI/Spawner/Spawner.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     [Header("Ref")]
     [SerializeField] private LightingManager lightingManager;
+    [SerializeField] private UI_Caution cautionUI;
 
     [Header("Spawner")]
     [SerializeField] private WaveData[] wave;
@@ -24,29 +25,42 @@ public class Spawner : MonoBehaviour
     [Header("Waypoints")]
     [SerializeField] private List<Transform> wayPoints = new List<Transform>();
 
+    private Coroutine spawnRoutine;
+    private bool wasNightLastFrame;
 
     private void Start()
     {
-        Debug.Log("Night: " + lightingManager._isNight);
         waveVar = 0;
         activeWave = wave[waveVar];
-        if(!testingMode)
+
+        if (testingMode)
         {
-            if (!lightingManager._isNight)
-            {
-                StartCoroutine(WaitForNight());
-            }
-            else if (lightingManager._isNight)
-            {              
-                StartWave();
-            }
-        }
-        else
-        {
-            StartCoroutine(spawnEnemy(activeWave.timeBetweenSpawn));
+            spawnRoutine = StartCoroutine(spawnEnemy(activeWave.timeBetweenSpawn));
         }
     }
 
+    private void Update()
+    {
+        bool isNight = lightingManager._isNight;
+
+        // Night just started → start wave if possible
+        if (isNight && !wasNightLastFrame)
+        {
+            TryStartWave();
+        }
+
+        // Night just ended → turn off caution UI
+        if (!isNight && wasNightLastFrame)
+        {
+            if (cautionUI.hasWaveStart)
+            {
+                cautionUI.hasWaveStart = false; // hide UI
+                Debug.Log("Night ended: Caution image turned off");
+            }
+        }
+
+        wasNightLastFrame = isNight;
+    }
     private IEnumerator spawnEnemy(float interval)
     {
         for (int i = 0; i < activeWave.EnemiesInWaves.Length; i++)
@@ -63,25 +77,19 @@ public class Spawner : MonoBehaviour
         OnWaveFinishedSpawning();
     }
 
-    private IEnumerator WaitForNight()
-    {
-        while (!lightingManager._isNight)
-        {
-            yield return null; // wait a frame
-        }
-
-        StartWave(); // fire exactly once
-    }
-
     void OnWaveFinishedSpawning()
     {
         waveInProgress = false;
+
+        if (cautionUI.hasWaveStart)
+            cautionUI.hasWaveStart = false; // turn off UI when wave finishes
+
         GoToNextWave();
     }
 
     private void GoToNextWave()
     {
-        if (waveVar == wave.Length - 1) // if at last wave
+        if (waveVar == wave.Length - 1) // last wave
         {
             spawningWave = false;
             return;
@@ -89,18 +97,26 @@ public class Spawner : MonoBehaviour
 
         waveVar++;
         activeWave = wave[waveVar];
-        StartWave();
+
+        // Only start next wave if it's night
+        if (lightingManager._isNight)
+            TryStartWave();
     }
 
-    public void StartWave()
+    public void TryStartWave()
     {
         if (!spawningWave || waveInProgress)
             return;
 
-        if (lightingManager._isNight)
-        {
-            waveInProgress = true;
-            StartCoroutine(spawnEnemy(activeWave.timeBetweenSpawn));
-        }
+        if (!lightingManager._isNight) // safety check
+            return;
+
+        if (!cautionUI.hasWaveStart)
+            cautionUI.hasWaveStart = true; // turn on UI
+        else
+            return; // already showing
+
+        waveInProgress = true;
+        StartCoroutine(spawnEnemy(activeWave.timeBetweenSpawn));
     }
 }
