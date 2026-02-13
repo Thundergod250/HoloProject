04fa2d283928f6e88aa2236714b91f20e81a49a0
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2026-02-13 14:20:41 +0800

    UPDATED Player UI
    Lighting Manager

--- Assets/Scripts/Scripts_World/Scripts_DayNightCycle/LightingManager.cs
+++ Assets/Scripts/Scripts_World/Scripts_DayNightCycle/LightingManager.cs
@@ -16,7 +16,7 @@ public class LightingManager : MonoBehaviour
     // Changed to 240 secs == 4 Mins per day 
     [SerializeField] private float _timeOfDay;
     [SerializeField] public bool _isNight = false;
-    [SerializeField] float _maxTimeOfDay = 240;
+    [SerializeField] float _maxTimeOfDay = 180;
 
     [SerializeField] protected Volume _hdriCubeSkyDay;
     [SerializeField] protected Volume _hdriCubeSkyNight;
@@ -67,7 +67,7 @@ public class LightingManager : MonoBehaviour
         // --- HDRI TRIGGER LOGIC ---
 
         // 1. Night to Day Trigger (60 to 65) // 0 - 150 Day
-        if (_timeOfDay >= 0 && _timeOfDay < 150) 
+        if (_timeOfDay >= 0 && _timeOfDay < 120) 
         {
             int targetIndex = _isMines ? 1 : (_isSnow ? 3 : 0);
 
@@ -79,7 +79,7 @@ public class LightingManager : MonoBehaviour
             _isNight = false;
         }
         // 2. Day to Night Trigger (240 to 245) // 221 - 360 Night
-        else if (_timeOfDay >= 151 && _timeOfDay < _maxTimeOfDay) 
+        else if (_timeOfDay >= 121 && _timeOfDay < _maxTimeOfDay) 
         {
             int targetIndex = 1; // Your Moonless/Night HDRI
 
