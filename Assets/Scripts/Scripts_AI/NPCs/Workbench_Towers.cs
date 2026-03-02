f227dd2066cd769e8c2083cf82959afe64a00ab2
Jeremy Kurt Tan <jeremykurttan@yahoo.com>
2026-03-03 00:03:20 +0800

    ADded cost to Unlock
    as of now 3 iron

--- Assets/Scripts/Scripts_AI/NPCs/Workbench_Towers.cs
+++ Assets/Scripts/Scripts_AI/NPCs/Workbench_Towers.cs
@@ -1,3 +1,4 @@
+using System.Resources;
 using TMPro;
 using UnityEngine;
 
@@ -6,26 +7,59 @@ public class Workbench_Towers : MonoBehaviour
     [Header("Ref")]
     [SerializeField] private TextMeshProUGUI upgradeText;
     [SerializeField] private TowerCategoryData_SO offensiveTowerData;
+    [SerializeField] private DropResourceManager gold;
+
+    [Header("Unlock Tower Vars")]
+    [SerializeField] private upgradeResourceType oreToSpend;
+    [SerializeField] private int towerUnlockCost;
     [SerializeField] private string towerUnlock;
+
     private bool playerInside = false;
+
+    private void Start()
+    {
+        upgradeText.enabled = false;
+    }
 
     private void Update()
     {
         if (playerInside && Input.GetKeyDown(KeyCode.F))
         {
-            UnlockTowers(towerUnlock);
+            UnlockTowers(towerUnlock, oreToSpend, towerUnlockCost);
         }
     }
 
-    public void UnlockTowers(string towerName)
+    public void UnlockTowers(string towerName, upgradeResourceType oreTypeToSpend, int customCost)
     {
         foreach (CardInfo card in offensiveTowerData.cards)
         {
-            if (card.towerName == towerName)
+            if (card.towerName != towerName)
+                continue;
+
+            // Already unlocked
+            if (!card.islocked)
             {
-                card.islocked = false;
-                break;
+                Debug.Log(card.towerName + " is already unlocked!");
+                return;
             }
+
+            // Check if player has enough of the chosen ore
+            int playerAmount = gold.GetResourceType(oreTypeToSpend);
+
+            if (playerAmount >= customCost)
+            {
+                gold.SpendingToResourceType(oreTypeToSpend, customCost);
+
+                // Unlock the tower
+                card.islocked = false;
+
+                Debug.Log("Unlocked " + card.towerName + " using " + customCost + " " + oreTypeToSpend);
+            }
+            else
+            {
+                Debug.Log("Not enough " + oreTypeToSpend + " to unlock " + card.towerName);
+            }
+            break;
         }
     }
 
