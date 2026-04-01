fe89787de3cc69b079edfd22656c9fd8f6af5132
Mark TImothy Sia <marktimothy.sia@benilde.edu.ph>
2026-04-01 22:59:30 +0800

    Beta Towershop Description
    Nerfed Button area for Description to work

--- Assets/Scripts/Scripts_AI/Towers/TowerCardManager.cs
+++ Assets/Scripts/Scripts_AI/Towers/TowerCardManager.cs
@@ -13,6 +13,8 @@ public class TowerCardManager : MonoBehaviour, IPoolable
     public GameObject TowerPrefab;
     public GameObject lockFilter;
 
+    public PageData towerPageData;
+
     private GameObject sourcePrefab;
     
     [Header("Tower Stats")]
@@ -23,6 +25,9 @@ public class TowerCardManager : MonoBehaviour, IPoolable
     public int FireRateIncrease;
 
     [SerializeField] public upgradeResourceType upgradeResourceType;
+    [SerializeField] private bool _enableDescription = false;
+    [SerializeField] private GameObject _descriptionHolderObj;
+    [SerializeField] private Image[] _hideUnHideImages;
 
     public int GetCostValue()
     {
@@ -33,13 +38,34 @@ public class TowerCardManager : MonoBehaviour, IPoolable
     {
         upgradeResourceType = info.neededUpgradeResourceType;
         TowerName.text = info.towerName;
-        Description.text = info.description;
+        //Description.text = info.description;
+
+        Description.text = info.pageData.pageDescription;
         OreCostImage.sprite = info.oreIcon;
         OreCost.text = info.oreCost.ToString();
         TowerImage.sprite = info.towerIcon;
         TowerPrefab = info.towerPrefab;
+        towerPageData = info.pageData;
     }
     
+    public void OnDescriptionButtonClicked()
+    {
+        if (!_enableDescription) // true
+        {
+            _descriptionHolderObj.SetActive(true);
+            _hideUnHideImages[1].gameObject.SetActive(true);
+            _hideUnHideImages[0].gameObject.SetActive(false);
+
+            _enableDescription = true;
+        }
+        else if(_enableDescription)
+        {
+            _descriptionHolderObj.SetActive(false);
+            _hideUnHideImages[1].gameObject.SetActive(false);
+            _hideUnHideImages[0].gameObject.SetActive(true);
+            _enableDescription = false;
+        }
+    }
 
     public void SetSourcePrefab(GameObject prefab) => sourcePrefab = prefab;
     public GameObject GetSourcePrefab() => sourcePrefab;
